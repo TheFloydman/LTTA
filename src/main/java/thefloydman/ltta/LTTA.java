1cf5aadca45e034d6a7ebb1ec71e2c037dc464a6
TheFloydman <danielwellsfloyd@gmail.com>
2018-06-15 15:16:11 -0700

    LTTA will now load *after* Guidebook.

--- src/main/java/thefloydman/ltta/LTTA.java
+++ src/main/java/thefloydman/ltta/LTTA.java
@@ -1,15 +1,15 @@
 package thefloydman.ltta;
 
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.registry.GameRegistry;
-import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.fml.common.Optional;
-
 import net.minecraftforge.items.ItemHandlerHelper;
+
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -17,23 +17,22 @@ import net.minecraft.util.ResourceLocation;
 
 import gigaherz.guidebook.client.BookRegistryEvent;
 
-@Mod(modid = LTTA.MODID, name = LTTA.NAME, version = LTTA.VERSION)
+@Mod(modid = LTTA.MOD_ID, name = LTTA.NAME, version = LTTA.VERSION, dependencies = "after:gbook")
 @Mod.EventBusSubscriber
 public class LTTA {
 	
-    public static final String MODID = "ltta";
+	public static final String MOD_ID = "ltta";
     public static final String NAME = "Linking Through the Ages";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.1.0";
     
-    // Register guidebook with Guidebook mod.
-    @Optional.Method(modid="gbook")
+	@Optional.Method(modid = "gbook")
     @SubscribeEvent
     public static void registerBook (BookRegistryEvent event) {
-    	event.register(new ResourceLocation(MODID + ":xml/ltta.xml"));
+    	event.register(new ResourceLocation(LTTA.MOD_ID + ":xml/ltta.xml"));
     }
     
     // Define guidebook ItemStack.
-    @GameRegistry.ItemStackHolder(value = "gbook:guidebook", nbt = "{Book:\"" + MODID + ":xml/ltta.xml\"}")
+    @GameRegistry.ItemStackHolder(value = "gbook:guidebook", nbt = "{Book:\"" + LTTA.MOD_ID + ":xml/ltta.xml\"}")
     public static ItemStack gbookStack;
 
     // Give one guidebook per player on first join.
@@ -42,7 +41,7 @@ public class LTTA {
     public static void checkGbookGiven(EntityJoinWorldEvent event)
     {
         final Entity entity = event.getEntity();
-        final String bookPlayerTag = MODID + ":bookGiven";
+        final String bookPlayerTag = LTTA.MOD_ID + ":bookGiven";
 
         if (entity instanceof EntityPlayer && !entity.getEntityWorld().isRemote && !entity.getTags().contains(bookPlayerTag))
         {

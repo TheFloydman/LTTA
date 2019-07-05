4542eecfc87c7247d6ff53f5d4a0d7f3cbd3bcd7
TheFloydman <danielwellsfloyd@gmail.com>
2019-07-05 12:00:01 -0700

    Adding more info.

--- src/main/java/thefloydman/ltta/LTTA.java
+++ src/main/java/thefloydman/ltta/LTTA.java
@@ -24,52 +24,57 @@ import thefloydman.ltta.config.ModConfig;
 @Mod(modid = LTTA.MOD_ID, name = LTTA.NAME, version = LTTA.VERSION, dependencies = "after:gbook;after:mystcraft")
 @Mod.EventBusSubscriber
 public class LTTA {
-	
+
 	public static final String MOD_ID = "ltta";
-    public static final String NAME = "Linking Through the Ages";
-    public static final String VERSION = "1.3.1";
-    
-    // Register LTTA and Narayani for Dummies with Guidebook.
+	public static final String NAME = "Linking Through the Ages";
+	public static final String VERSION = "1.4.0";
+
+	// Register LTTA and Narayani for Dummies with Guidebook.
 	@Optional.Method(modid = "gbook")
-    @SubscribeEvent
-    public static void registerBook (BookRegistryEvent event) {
-    	event.register(new ResourceLocation(LTTA.MOD_ID + ":xml/ltta.xml"));
-    	event.register(new ResourceLocation(LTTA.MOD_ID + ":xml/narayan_book.xml"));
-    }
-    
-    // Define LTTA ItemStack.
-    @GameRegistry.ItemStackHolder(value = "gbook:guidebook", nbt = "{Book:\"" + LTTA.MOD_ID + ":xml/ltta.xml\"}")
-    public static ItemStack lttaStack;
-
-    // Give one LTTA book per player on first join (unless disabled in config).
-    @Optional.Method(modid = "mystcraft")
-    @SubscribeEvent
-    public static void checkGbookGiven(EntityJoinWorldEvent event) {
-        final Entity entity = event.getEntity();
-        final String bookPlayerTag = LTTA.MOD_ID + ":bookGiven";
-
-        if (entity instanceof EntityPlayer && !entity.getEntityWorld().isRemote && !entity.getTags().contains(bookPlayerTag) && ModConfig.giveBookOnFirstSpawn == true) {
-            ItemHandlerHelper.giveItemToPlayer((EntityPlayer) entity, lttaStack.copy());
-            entity.addTag(bookPlayerTag);
-        } else if (ModConfig.giveBookOnFirstSpawn == false) {
-        	entity.addTag(bookPlayerTag);
-        }
-    }
-	
-    // Add Narayani book to Mystcraft loot table.
-    @Optional.Method(modid = "mystcraft")
 	@SubscribeEvent
-	public static void onLootTableLoad(LootTableLoadEvent event) {
-            if(event.getName().toString().equals("mystcraft:mystcraft_treasure")) {
-            	LootEntry customEntry = event.getLootTableManager().getLootTableFromLocation(new ResourceLocation("ltta", "loot_narayan_book")).getPool("main").getEntry("narayani_mystcraft");
-            	event.getTable().getPool("mystcraft:mystcraft_treasure").addEntry(customEntry);
-            }
+	public static void registerBook(BookRegistryEvent event) {
+		event.register(new ResourceLocation(LTTA.MOD_ID + ":xml/ltta.xml"));
+		event.register(new ResourceLocation(LTTA.MOD_ID + ":xml/narayan_book.xml"));
 	}
 
-    @EventHandler
-    public void preInit(FMLPreInitializationEvent event) {}
+	// Define LTTA ItemStack.
+	@GameRegistry.ItemStackHolder(value = "gbook:guidebook", nbt = "{Book:\"" + LTTA.MOD_ID + ":xml/ltta.xml\"}")
+	public static ItemStack lttaStack;
 
-    @EventHandler
-    public void init(FMLInitializationEvent event) {}    
-    
+	// Give one LTTA book per player on first join (unless disabled in config).
+	@Optional.Method(modid = "mystcraft")
+	@SubscribeEvent
+	public static void checkGbookGiven(EntityJoinWorldEvent event) {
+		final Entity entity = event.getEntity();
+		final String bookPlayerTag = LTTA.MOD_ID + ":bookGiven";
+
+		if (entity instanceof EntityPlayer && !entity.getEntityWorld().isRemote
+				&& !entity.getTags().contains(bookPlayerTag) && ModConfig.giveBookOnFirstSpawn == true) {
+			ItemHandlerHelper.giveItemToPlayer((EntityPlayer) entity, lttaStack.copy());
+			entity.addTag(bookPlayerTag);
+		} else if (ModConfig.giveBookOnFirstSpawn == false) {
+			entity.addTag(bookPlayerTag);
+		}
+	}
+
+	// Add Narayani book to Mystcraft loot table.
+	@Optional.Method(modid = "mystcraft")
+	@SubscribeEvent
+	public static void onLootTableLoad(LootTableLoadEvent event) {
+		if (event.getName().toString().equals("mystcraft:mystcraft_treasure")) {
+			LootEntry customEntry = event.getLootTableManager()
+					.getLootTableFromLocation(new ResourceLocation("ltta", "loot_narayan_book")).getPool("main")
+					.getEntry("narayani_mystcraft");
+			event.getTable().getPool("mystcraft:mystcraft_treasure").addEntry(customEntry);
+		}
+	}
+
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+	}
+
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+	}
+
 }
